9a4406cc8f8f36840d9f336f01e951588f49507d
Kai Siang <kslui99@gmail.com>
2022-09-16 01:24:52 +0800

    fix: add descriptive response message for unverified email

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,22 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _
+from rest_framework import exceptions
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as JwtTokenObtainPairSerializer
 
 from .models import User
 
 class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
+    default_error_messages = {
+        'nus_email_unverified': _('NUS Email is not verified')
+    }
+
     def validate(self, attrs):
         nus_email = attrs.get('nus_email')
         user = User.objects.filter(nus_email=nus_email).first()
         
-        if not user.is_verified:
-            return None
+        if user and not user.is_verified:
+            raise exceptions.AuthenticationFailed(
+                self.error_messages['nus_email_unverified'],
+                'nus_email_unverified',
+            )
 
         return super().validate(attrs)
