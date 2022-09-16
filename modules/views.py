55ec6b1cc5ea09be2f1847914314e3f690a0442b
Bernard Wan <bernardwan99@gmail.com>
2022-09-17 06:21:39 +0800

    Add search functionality for modules endpoint

--- modules/views.py
+++ modules/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from rest_framework import viewsets
 
@@ -5,6 +6,11 @@ from .serializers import ModuleSerializer
 from .models import Module
 
 class ModuleViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Module.objects.all().order_by('module_code')
     serializer_class = ModuleSerializer
-    
+
+    def get_queryset(self):
+        queryset = Module.objects.all().order_by('module_code')
+        search_query = self.request.query_params.get('q')
+        if search_query is not None:
+            queryset = queryset.filter(Q(title__icontains=search_query) | Q(module_code__icontains=search_query))
+        return queryset
