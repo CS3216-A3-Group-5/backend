7b1b711a1896cf989e2cbadb2b7acd1d9bddad16
Bernard Wan <bernardwan99@gmail.com>
2022-09-19 05:48:26 +0800

    Add module/users endpoint (untested)

--- modules/views.py
+++ modules/views.py
@@ -1,11 +1,16 @@
+from urllib import request
 from django.db.models import Q
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import ModuleSerializer
-from users.serializers import SimpleUserSerializer
+from users.serializers import SimpleUserSerializer, EnrolmentSerializer, UserSerializer
 from .models import Module
 from users.models import User, Enrolment, Connections
+
+from modules import serializers
 
 class ModuleViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ModuleSerializer
@@ -19,14 +24,21 @@ class ModuleViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 class ModuleUsersViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = SimpleUserSerializer
+    serializer_class = SimpleUserSerializer()
+    permission_classes = [permissions.IsAuthenticated]
 
-    
-    #requester = request.user
 
     def get_queryset(self):
+        print(self.request.user)
         queryset = Enrolment.objects.all()
         module_code = self.kwargs['module_code']
-        queryset = queryset.filter(module__iexact=module_code)
+        queryset = queryset.filter(module__module_code__iexact=module_code)
         return queryset
+
+class ModuleUsersView(APIView):
+    #permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, module_code, format=None):
+        enrolments = Enrolment.objects.all().filter(module__module_code__iexact=module_code)
+        serializer = SimpleUserSerializer(enrolments, many=True, context={'user': request.user})
+        return Response(serializer.data)
 
--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,15 @@
 from unicodedata import name
+from django.contrib.auth.models import AnonymousUser
+from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions
+from rest_framework.fields import CurrentUserDefault
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as JwtTokenObtainPairSerializer
 
 from rest_framework import serializers
 
-from .models import User
+from .models import Connections, User, Enrolment
+
 
 class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
     default_error_messages = {
@@ -25,11 +29,13 @@ class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
         return super().validate(attrs)
 
 class SimpleUserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=50, source='user.name')
+    name = serializers.CharField(max_length=50, source='user.username')
     user_status = serializers.SerializerMethodField() #serializers.IntegerField() # based on module
     connection_status = serializers.SerializerMethodField() #serializers.IntegerField() #based on module and user token
+    #connection_status = serializers.PrimaryKeyRelatedField(queryset = )
+    #print(CurrentUserDefault())
 
-    def user_status(self, obj):
+    def get_user_status(self, obj):
         if obj.status == 'LF':
             return 1
         elif obj.status == 'WH':
@@ -37,10 +43,41 @@ class SimpleUserSerializer(serializers.Serializer):
         else:
             return 0
         
-    def connection_status(self, obj):
-        # get Connections table filtered by sender or receiver being user making api request
+    def get_connection_status(self, obj):
+        # get Connections table filtered by requester or accepter being user making api request
         # for each user, check if in this filtered table
         # if in table, return connection status
         # else no connection
+        user = None
+        user = self.context.get("user")
+        
+        if not user.id:
+            return 0
+
+        queryset = Connections.objects.filter(Q(requester=user) | Q(accepter=user))
+        try:
+            record = queryset.get(Q(requester__exact=obj.user) | Q(accepter__exact=obj))
+            if record.status == 'AC':
+                return 2
+            elif record.status == 'PD':
+                return 1
+            else:
+                return 0
+
+        except:
+            return 20
+
         return 0
     
+class UserSerializer(serializers.ModelSerializer):
+    #enrolment = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ('username, nus_email', 'enrolment')
+
+class EnrolmentSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.username')
+    class Meta:
+        model = Enrolment
+        fields = ('user', 'module', 'status')
+        depth = 1
