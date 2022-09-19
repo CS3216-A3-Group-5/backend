d37f01d083cdee8feee762cbde83e7ae349e1309
Bernard Wan <bernardwan99@gmail.com>
2022-09-19 23:18:17 +0800

    Remove comment

--- modules/urls.py
+++ modules/urls.py
@@ -4,6 +4,5 @@ from modules import views
 
 urlpatterns = [
     path('modules/', views.ModuleViewSet.as_view({'get': 'list'}), name='get-modules'),
-    #path('modules/<module_code>/users', views.ModuleUsersViewSet.as_view({'get': 'list'}), name='get-module-users'),
     path('modules/<module_code>/users', views.ModuleUsersView.as_view(), name='get-module-users'),
 ]
