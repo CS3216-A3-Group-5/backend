4d7d15efc4bb18a64156db58aa8e27a55b73b775
kslui99 <40225831+kslui99@users.noreply.github.com>
2022-09-19 17:09:30 +0800

    feat(authentication): update login for integration with frontend (#3)
    * fix: revert change in Module model primary key
    
    * feat: invalidate refresh token once it is used
    
    * feat: change access and refresh tokens expiry duration
    
    * feat: implement /token/verify/, to verify validity of refresh or access token
    
    * fix: blacklist old refresh tokens
    
    * feat: add login to browsable API
    
    * feat: implement error messages for login
    
    * fix: tighten security for invalid login credentials

--- users/serializers.py
+++ users/serializers.py
@@ -1,26 +1,26 @@
-from unicodedata import name
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import authenticate
 from rest_framework import exceptions
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as JwtTokenObtainPairSerializer
-
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as JwtTokenObtainPairSerializer
 
 from .models import User
 
 class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
-    default_error_messages = {
-        'nus_email_unverified': _('NUS Email is not verified')
-    }
-
     def validate(self, attrs):
         nus_email = attrs.get('nus_email')
-        user = User.objects.filter(nus_email=nus_email).first()
+        password = attrs.get('password')
+        try:
+            request = self.context["request"]
+        except KeyError:
+            pass
         
-        if user and not user.is_verified:
-            raise exceptions.AuthenticationFailed(
-                self.error_messages['nus_email_unverified'],
-                'nus_email_unverified',
-            )
+        user = authenticate(request, nus_email=nus_email, password=password)
+
+        if not user:
+            return {
+                'error_code': 1,
+                'error_message': 'Email and/or password is incorrect.'
+            }
 
         return super().validate(attrs)
 
