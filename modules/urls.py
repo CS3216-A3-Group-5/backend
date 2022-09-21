0bfc82be75bc850d3f802a1a808bf2133d8699c2
Kai Siang <kslui99@gmail.com>
2022-09-22 00:30:01 +0800

    fix: remove all trialing / in urls

--- modules/urls.py
+++ modules/urls.py
@@ -3,9 +3,9 @@ from rest_framework_simplejwt import views
 from modules import views
 
 urlpatterns = [
-    path('modules/', views.ModulesView.as_view(), name='get-modules'),
-    path('modules/<module_code>/', views.ModuleView.as_view(), name='get-module'),
-    path('modules/<module_code>/users/', views.ModuleUsersView.as_view(), name='get-module-users'),
-    path('modules/update/<academic_year>/', views.ModuleUpdateView.as_view(), name='update-modules'),
-    path('modules/update/manual/<academic_year>/', views.ModuleManualUpdateView.as_view(), name='update-modules'),
+    path('modules', views.ModulesView.as_view(), name='get-modules'),
+    path('modules/<module_code>', views.ModuleView.as_view(), name='get-module'),
+    path('modules/<module_code>/users', views.ModuleUsersView.as_view(), name='get-module-users'),
+    path('modules/update/<academic_year>', views.ModuleUpdateView.as_view(), name='update-modules'),
+    path('modules/update/manual/<academic_year>', views.ModuleManualUpdateView.as_view(), name='update-modules'),
 ]
