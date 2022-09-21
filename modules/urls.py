eec33087bb2546b685e852e2ac0eb5e312160292
Bernard Wan <bernardwan99@gmail.com>
2022-09-22 04:50:59 +0800

    Fix regex

--- modules/urls.py
+++ modules/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     re_path(r'modules/?$', views.ModulesView.as_view(), name='get-modules'),
     re_path(r'modules/(?P<module_code>\w+)/?$', views.ModuleView.as_view(), name='get-module'),
     re_path(r'modules/(?P<module_code>\w+)/users/?$', views.ModuleUsersView.as_view(), name='get-module-users'),
-    re_path(r'modules/update/(?P<academic_year>\w+)/?$', views.ModuleUpdateView.as_view(), name='update-modules'),
-    re_path(r'modules/update/manual/(?P<academic_year>\w+)/?$', views.ModuleManualUpdateView.as_view(), name='update-modules'),
+    re_path(r'modules/update/(?P<academic_year>[\w-]+)/?$', views.ModuleUpdateView.as_view(), name='update-modules'),
+    re_path(r'modules/update/manual/(?P<academic_year>[\w-]+)/?$', views.ModuleManualUpdateView.as_view(), name='update-modules'),
 ]
